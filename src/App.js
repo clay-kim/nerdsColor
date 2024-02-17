ff789ca6a3f4498db9ccd4e0d348b4729ec1150e
Sung Kim <clay1013@gmail.com>
2024-02-17 15:48:38 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Canvas from "./Canvas/Canvas";
+import BackgroundText from "./BackgroundText";
+
+function App() {
+  return (
+    <div className="App">
+    <BackgroundText/>  
+    <Canvas />
+      
+    </div>
+  );
+}
+export default App;
---
+++ src/BackgroundText.js
@@ -0,0 +1,13 @@
+import './style.css'
+import img from './nerds_rainbow.png'
+
+export default function Testing(){
+return(
+    <div className="container">
+    <img src={img} className='pic' alt='main' />
+        <h1 className="text">NeRdS</h1>
+    </div>
+
+);
+
+}
