186fd87047eaae4cdd5c9a5f3bd2ff7677c63826
Sung Kim <clay1013@gmail.com>
2024-02-17 16:08:03 -0800

    glowImg

--- src/BackgroundText.js
+++ src/BackgroundText.js
@@ -4,7 +4,7 @@ import img from './nerds_rainbow.png'
 export default function Testing(){
 return(
     <div className="container">
-    <img src={img} className='pic' alt='main' />
+    <img src={img} id='pic' alt='main' />
         <h1 className="text">NeRdS</h1>
     </div>
 
